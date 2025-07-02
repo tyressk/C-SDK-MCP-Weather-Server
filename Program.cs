20b44575e251476ca9c2feb3090128714407dd8f
Tyress K <tyressk@pm.me>
2025-07-02 13:34:50 +0900

    Working C# server implementation

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using QuickstartWeatherServer.Tools;
+using System.Net.Http.Headers;
+//based on samples from https://github.com/modelcontextprotocol/csharp-sdk
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMcpServer()
+    .WithHttpTransport()
+    .WithTools<WeatherTools>();
+
+builder.Services.AddSingleton(_ =>
+{
+    var client = new HttpClient() { BaseAddress = new Uri("https://api.weather.gov") };
+    client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("weather-tool", "1.0"));
+    return client;
+});
+
+var app = builder.Build();
+Console.WriteLine("Running");
+app.MapMcp();
+await app.RunAsync();
